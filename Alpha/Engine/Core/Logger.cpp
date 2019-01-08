34ef05e922ffe924ab922147969e2b945693d372
MidnightSoftwares <midnightsoftwares@gmail.com>
2019-01-08 01:22:53 +0100

    Added and quick fixed logger file name output.

--- Alpha/Engine/Core/Logger.cpp
+++ Alpha/Engine/Core/Logger.cpp
@@ -1,11 +1,11 @@
 ﻿#include "Logger.h"
 
-//#include <Shlwapi.h>
+#include <Shlwapi.h>
 
 #include <sstream>
 
 void Logger::Log(LogLevel level, const std::wstring& message,
-    const wchar_t* /* file */, unsigned long /* line */)
+    const wchar_t* file, unsigned long line)
 {
     const auto levelStr = [level]() -> std::wstring
     {
@@ -22,17 +22,17 @@ void Logger::Log(LogLevel level, const std::wstring& message,
         }
     }();
 
-    //const auto fileStr = [file, line]() -> std::wstring
-    //{
-    //    return PathFindFileName(file) + L'(' + std::to_wstring(line) + L')';
-    //}();
+    const auto fileStr = [file, line]() -> std::wstring
+    {
+        return std::wstring{PathFindFileName(file)} + L':' + std::to_wstring(line);
+    }();
 
-    const auto logStr = [&levelStr, &message]() -> std::wstring
+    const auto logStr = [&levelStr, &fileStr, &message]() -> std::wstring
     {
         std::wstringstream ss;
-        ss << L'[' << levelStr << L"] " << message << L'\n';
+        ss << L'[' << levelStr << L"][" << fileStr << L"] " << message << L'\n';
         return ss.str();
     }();
 
-    OutputDebugStringW(logStr.c_str());
+    OutputDebugString(logStr.c_str());
 }
